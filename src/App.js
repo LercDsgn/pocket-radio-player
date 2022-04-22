8f54d839eca16ea4a52e3a5d4602f431b1b1ba09
ACRZeuss <69459810+ACRZeuss@users.noreply.github.com>
2022-04-23 02:57:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { DATA } from "./components/Data";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import "./App.css";
 
 function App() {
+  const [url, setUrl] = useState(null);
+
+  const changeUrl = (url) => {
+    setUrl(url);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header">Pocket Radio Player</div>
+      <div className="App">
+        {DATA.map((item) => {
+          return (
+            <a onClick={() => setUrl(item.url)} className="radio-station">
+              <img src={item.image} />
+            </a>
+          );
+        })}
+      </div>
+      <div className="player">
+        <AudioPlayer
+          autoPlay
+          preload={'none'}
+          showSkipControls={false}
+          showJumpControls={false}
+          src={url}
+          onPlay={(e) => console.log("onPlay")}
+          // other props here
+        />
+      </div>
     </div>
   );
 }
