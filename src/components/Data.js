aeed893c27f17d5ab187f44b2286460fc9f13e1e
ACRZeuss <69459810+ACRZeuss@users.noreply.github.com>
2022-04-23 13:44:14 +0300

    update

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,13 +1,68 @@
 export const DATA = [
-    { id: 1, name: "Best FM", url: 'http://officialbestfm.radyotvonline.net/bestfmofficial', image: 'https://muzikonair.com/wp-content/uploads/2019/09/best-fm-yeni-logo-1024x576.jpg' },
-    { id: 2, name: "Alem FM", url: 'https://turkmedya.radyotvonline.net/alemfmaac', image: 'https://yt3.ggpht.com/ytc/AKedOLTLli275fw6X62J2izsZ3TF8pHV2Mfkt--rhk6FvQ=s900-c-k-c0x00ffffff-no-rj' },
-    { id: 3, name: "JoyTürk FM", url: 'https://playerservices.streamtheworld.com/api/livestream-redirect/JOY_TURK2.mp3', image: 'https://mediacdns.karnaval.com/media/station_media/22/logos/android_default_logo.png?v=071118032122' },
-    { id: 4, name: "Kafa Radyo", url: 'https://37.247.98.17/;', image: 'https://i1.sndcdn.com/artworks-u1FJHUHAQsV9kudC-Dv1puQ-t500x500.jpg' },
-    { id: 5, name: "Kral FM", url: 'https://ssldyg2.radyotvonline.com/dyg/kralfm64.stream/playlist.m3u8', image: 'https://radyodi.com/wp-content/uploads/2019/02/kralfmt.png' },
-    { id: 6, name: "Kral Pop Radyo", url: 'https://dygedge.radyotvonline.net/kralpop/playlist.m3u8', image: 'https://cdn1.kralmuzik.com.tr/media/content/19-05/20/kralpop.png' },
-    { id: 7, name: "PowerTürk FM", url: 'https://mpegpowerturk.listenpowerapp.com/powerturk/mpeg/icecast.audio', image: 'https://cdn.powergroup.com.tr/powerapp/channels/big/powerTurk.png?v=626' },
-    { id: 8, name: "Radyo 45'lik", url: 'https://stream.radyo45lik.com:4545/', image: 'https://upload.wikimedia.org/wikipedia/tr/thumb/8/80/Radyo_45%27lik_logosu.png/1200px-Radyo_45%27lik_logosu.png' },
-    { id: 9, name: "Show Radyo", url: 'https://windows.showradyo.com.tr/;', image: 'https://pbs.twimg.com/profile_images/1264909493118394368/t8_Hk20n_400x400.jpg' },
-    { id: 10, name: "SlowTürk Radyo", url: 'https://radyo.duhnet.tv/slowturk', image: 'https://yt3.ggpht.com/b-eTgHzrQ-04YZfLvqKe-JMdMCWeShcULyaPwnzSYbhj4Zf8solOUB3q-kxpLdLzP5XCa5GayA=s900-c-k-c0x00ffffff-no-rj' },
-    { id: 11, name: "Radyo Viva", url: 'https://moondigitaledge.radyotvonline.net/radyoviva/playlist.m3u8', image: 'https://www.logovector.org/wp-content/uploads/logos/png/r/radyo_viva_logo.png' },
+    {
+        id: 1,
+        name: "Best FM",
+        url: 'https://best.canli-radyo.biz/;stream.mp3',
+        image: 'https://muzikonair.com/wp-content/uploads/2019/09/best-fm-yeni-logo-1024x576.jpg'
+    },
+    {
+        id: 2,
+        name: "Alem FM",
+        url: 'https://turkmedya.radyotvonline.net/alemfmaac',
+        image: 'https://yt3.ggpht.com/ytc/AKedOLTLli275fw6X62J2izsZ3TF8pHV2Mfkt--rhk6FvQ=s900-c-k-c0x00ffffff-no-rj'
+    },
+    {
+        id: 3,
+        name: "JoyTürk FM",
+        url: 'https://playerservices.streamtheworld.com/api/livestream-redirect/JOY_TURK2.mp3',
+        image: 'https://mediacdns.karnaval.com/media/station_media/22/logos/android_default_logo.png?v=071118032122'
+    },
+    {
+        id: 4,
+        name: "Kafa Radyo",
+        url: 'https://37.247.98.17/;',
+        image: 'https://i1.sndcdn.com/artworks-u1FJHUHAQsV9kudC-Dv1puQ-t500x500.jpg'
+    },
+    {
+        id: 5,
+        name: "Kral FM",
+        url: 'https://ssldyg2.radyotvonline.com/dyg/kralfm64.stream/playlist.m3u8',
+        image: 'https://radyodi.com/wp-content/uploads/2019/02/kralfmt.png'
+    },
+    {
+        id: 6,
+        name: "Kral Pop Radyo",
+        url: 'https://dygedge.radyotvonline.net/kralpop/playlist.m3u8',
+        image: 'https://cdn1.kralmuzik.com.tr/media/content/19-05/20/kralpop.png'
+    },
+    {
+        id: 7,
+        name: "PowerTürk FM",
+        url: 'https://mpegpowerturk.listenpowerapp.com/powerturk/mpeg/icecast.audio',
+        image: 'https://cdn.powergroup.com.tr/powerapp/channels/big/powerTurk.png?v=626'
+    },
+    {
+        id: 8,
+        name: "Radyo 45'lik",
+        url: 'https://stream.radyo45lik.com:4545/',
+        image: 'https://upload.wikimedia.org/wikipedia/tr/thumb/8/80/Radyo_45%27lik_logosu.png/1200px-Radyo_45%27lik_logosu.png'
+    },
+    {
+        id: 9,
+        name: "Show Radyo",
+        url: 'https://windows.showradyo.com.tr/;',
+        image: 'https://pbs.twimg.com/profile_images/1264909493118394368/t8_Hk20n_400x400.jpg'
+    },
+    {
+        id: 10,
+        name: "SlowTürk Radyo",
+        url: 'https://radyo.duhnet.tv/slowturk',
+        image: 'https://yt3.ggpht.com/b-eTgHzrQ-04YZfLvqKe-JMdMCWeShcULyaPwnzSYbhj4Zf8solOUB3q-kxpLdLzP5XCa5GayA=s900-c-k-c0x00ffffff-no-rj'
+    },
+    {
+        id: 11,
+        name: "Radyo Viva",
+        url: 'https://moondigitaledge.radyotvonline.net/radyoviva/playlist.m3u8',
+        image: 'https://www.logovector.org/wp-content/uploads/logos/png/r/radyo_viva_logo.png'
+    },
 ]
