93f8103c913fcd39e807e21756ca07441a910232
ACRZeuss <69459810+ACRZeuss@users.noreply.github.com>
2022-04-23 23:27:01 +0300

    update

--- src/components/Data.js
+++ src/components/Data.js
@@ -60,13 +60,13 @@ export const DATA = [
     image:
       "https://upload.wikimedia.org/wikipedia/tr/thumb/8/80/Radyo_45%27lik_logosu.png/1200px-Radyo_45%27lik_logosu.png",
   },
-  {
-    id: 9,
-    name: "Show Radyo",
-    url: "http://windows.showradyo.com.tr/;stream.mp3",
-    image:
-      "https://pbs.twimg.com/profile_images/1264909493118394368/t8_Hk20n_400x400.jpg",
-  },
+  // {
+  //   id: 9,
+  //   name: "Show Radyo",
+  //   url: "http://windows.showradyo.com.tr/;stream.mp3",
+  //   image:
+  //     "https://pbs.twimg.com/profile_images/1264909493118394368/t8_Hk20n_400x400.jpg",
+  // },
   {
     id: 10,
     name: "SlowTürk Radyo",
@@ -74,13 +74,13 @@ export const DATA = [
     image:
       "https://yt3.ggpht.com/b-eTgHzrQ-04YZfLvqKe-JMdMCWeShcULyaPwnzSYbhj4Zf8solOUB3q-kxpLdLzP5XCa5GayA=s900-c-k-c0x00ffffff-no-rj",
   },
-  {
-    id: 11,
-    name: "Radyo Viva",
-    url: "http://46.20.3.231/;",
-    image:
-      "https://www.logovector.org/wp-content/uploads/logos/png/r/radyo_viva_logo.png",
-  },
+  // {
+  //   id: 11,
+  //   name: "Radyo Viva",
+  //   url: "http://46.20.3.231/;",
+  //   image:
+  //     "https://www.logovector.org/wp-content/uploads/logos/png/r/radyo_viva_logo.png",
+  // },
   {
     id: 12,
     name: "Dream Türk",
@@ -95,34 +95,34 @@ export const DATA = [
     image:
       "https://mediacdns.karnaval.com/media/station_media/2/logos/meta_image.png",
   },
-  {
-    id: 14,
-    name: "Pal Station",
-    url: "http://shoutcast.radyogrup.com:1020/;",
-    image:
-      "https://image.winudf.com/v2/image1/Y29tLnBhbF9zdGF0aW9uLnJhZGlvX3NjcmVlbl81XzE1NDkzNDc5MThfMDQw/screen-5.jpg?fakeurl=1&type=.webp",
-  },
-  {
-    id: 15,
-    name: "Pal Akustik",
-    url: "http://shoutcast.radyogrup.com:2030/;",
-    image:
-      "https://is5-ssl.mzstatic.com/image/thumb/Purple91/v4/70/ee/b6/70eeb6e4-9c7b-27a2-4ff1-8b47807237cf/source/512x512bb.jpg",
-  },
-  {
-    id: 16,
-    name: "Pal Slow",
-    url: "http://shoutcast.radyogrup.com:2020/;",
-    image:
-      "https://play-lh.googleusercontent.com/lYPNMkH0s78_H6Vf7l2X93L4AZ2BS0TwfaalzM1DmJxCOaF_gAk69giWcearVepw8dU",
-  },
-  {
-    id: 17,
-    name: "Pal Nostalji",
-    url: "http://shoutcast.radyogrup.com:1010/;",
-    image:
-      "https://image.winudf.com/v2/image1/Y29tLnBhbG5vc3RhbGppLnJhZGlvX3NjcmVlbl8wXzE1NDg2MDc4NTlfMDA3/screen-0.jpg?h=500&fakeurl=1&type=.jpg",
-  },
+  // {
+  //   id: 14,
+  //   name: "Pal Station",
+  //   url: "http://shoutcast.radyogrup.com:1020/;",
+  //   image:
+  //     "https://image.winudf.com/v2/image1/Y29tLnBhbF9zdGF0aW9uLnJhZGlvX3NjcmVlbl81XzE1NDkzNDc5MThfMDQw/screen-5.jpg?fakeurl=1&type=.webp",
+  // },
+  // {
+  //   id: 15,
+  //   name: "Pal Akustik",
+  //   url: "http://shoutcast.radyogrup.com:2030/;",
+  //   image:
+  //     "https://is5-ssl.mzstatic.com/image/thumb/Purple91/v4/70/ee/b6/70eeb6e4-9c7b-27a2-4ff1-8b47807237cf/source/512x512bb.jpg",
+  // },
+  // {
+  //   id: 16,
+  //   name: "Pal Slow",
+  //   url: "http://shoutcast.radyogrup.com:2020/;",
+  //   image:
+  //     "https://play-lh.googleusercontent.com/lYPNMkH0s78_H6Vf7l2X93L4AZ2BS0TwfaalzM1DmJxCOaF_gAk69giWcearVepw8dU",
+  // },
+  // {
+  //   id: 17,
+  //   name: "Pal Nostalji",
+  //   url: "http://shoutcast.radyogrup.com:1010/;",
+  //   image:
+  //     "https://image.winudf.com/v2/image1/Y29tLnBhbG5vc3RhbGppLnJhZGlvX3NjcmVlbl8wXzE1NDg2MDc4NTlfMDA3/screen-0.jpg?h=500&fakeurl=1&type=.jpg",
+  // },
   {
     id: 18,
     name: "Virgin Radio",
